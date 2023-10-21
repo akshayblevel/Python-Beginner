2fa6ccefc1381cd05db3552c64cd70c694a614b6
Akshay Patel <akshayblevel@gmail.com>
2023-10-21 20:42:19 +0530

    User Input

---
+++ 16_userinput.py
@@ -0,0 +1,14 @@
+x = input("Enter 1st number:")  # input always gives value in str
+y = input("Enter 2nd number:")
+
+z = int(x) + int(y)
+
+print(z)
+
+ch = input("Enter a character:")
+print(ch)
+print(ch[0])  # print first character if more than one character is provided
+
+
+result = eval(input("Enter an expression:"))  # e.g 2 + 3 - 1
+print(result)
