c0401fc7141db6594bbe417e84f52972f01c2ff7
Akshay Patel <akshayblevel@gmail.com>
2023-10-22 13:12:04 +0530

    For loop

---
+++ 19_forloop.py
@@ -0,0 +1,28 @@
+# print each value from list
+lst = ['Akshay', 42, 'Panth', 6]
+
+for value in lst:
+    print(value)
+
+# print each character from string
+
+lst = 'Akshay'
+
+for ch in lst:
+    print(ch)
+
+# print range from 1 to 9
+
+for i in range(10):
+    print(i)
+
+# print range from 11 to 20 by skipping 2
+
+for i in range(11, 20, 2):
+    print(i)
+
+# print range in reverse order
+
+for i in range(20, 11, -1):
+    print(i)
+
