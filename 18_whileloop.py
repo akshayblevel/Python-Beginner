27bd6375c3373e9fa5aab1325ac5bf536138f7d5
Akshay Patel <akshayblevel@gmail.com>
2023-10-22 13:00:47 +0530

    While Loop

---
+++ 18_whileloop.py
@@ -0,0 +1,33 @@
+i = 1                           # Initialization
+
+while i <= 5:                   # Condition
+    print("Hello Akshay")
+    i = i + 1                   # Increment
+
+# ----------------------------------------------
+i = 5                           # Initialization
+
+while i >= 1:                   # Condition
+    print("Hello Patel")
+    i = i - 1                   # Decrement
+
+# ----------------------------------------------
+
+i = 1
+j = 1
+
+while i <= 5:
+    print("Hello Akshay ", end="")
+    j = 1
+    while j <= 5:
+        print("Patel ", end="")
+        j = j + 1
+
+    i = i + 1
+    print()
+
+# Hello Akshay Patel Patel Patel Patel Patel
+# Hello Akshay Patel Patel Patel Patel Patel
+# Hello Akshay Patel Patel Patel Patel Patel
+# Hello Akshay Patel Patel Patel Patel Patel
+# Hello Akshay Patel Patel Patel Patel Patel
