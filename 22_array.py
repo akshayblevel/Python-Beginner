10697895035a92addac5dbbd129c502c76bb64f5
Akshay Patel <akshayblevel@gmail.com>
2023-10-27 07:32:16 +0530

    Array

--- 22_array.py
+++ 22_array.py
@@ -13,8 +13,52 @@ from array import *
 #   'f'       float             float               4
 #   'd'       double            float               8
 
-vals = array('i',[2, 5, 8, 11, 18])
+vals = array('i', [2, 5, 8, 11, 18])
+
+# Get the address and size of an array
 print(vals.buffer_info())  # (3090525830256, 5) (address, size)
 
+for i in range(len(vals)):
+    print(vals[i])
 
+# 2
+# 5
+# 8
+# 11
+# 18
 
+for e in vals:
+    print(e)
+
+# 2
+# 5
+# 8
+# 11
+# 18
+
+# Print single element of an array
+print(vals[1])  # 5
+
+# Copy array in new array
+
+newVals = array(vals.typecode, (a for a in vals))
+
+for e in newVals:
+    print(e)
+
+# Reverse the array
+vals.reverse()
+print(vals)  # array('i', [18, 11, 8, 5, 2])
+
+# Character
+charArr = array('u', ['a', 'k', 's', 'h', 'a', 'y'])
+
+for e in charArr:
+    print(e)
+
+# a
+# k
+# s
+# h
+# a
+# y
