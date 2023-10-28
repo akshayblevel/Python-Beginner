2bfb8f661c6ea8a74e64e18f70c1c38db37f92c6
Akshay Patel <akshayblevel@gmail.com>
2023-10-28 14:26:23 +0530

    Function

---
+++ 27_function.py
@@ -0,0 +1,20 @@
+
+def addition(x, y):
+    z = x + y
+    return z
+
+
+result = addition(10, 12)
+print(result)  # 22
+
+
+def add_sub(x, y):
+    a = x + y
+    s = x - y
+    return a, s
+
+
+res, res1 = add_sub(10, 12)
+print(res)  # 22
+print(res1)  # -2
+print(res, res1)  # 22 -2
