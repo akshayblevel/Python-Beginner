6536a5d2ff36f87c7dbd61da605f5ce837c75e34
Akshay Patel <akshayblevel@gmail.com>
2023-10-29 13:03:26 +0530

    Module

---
+++ 35_module.py
@@ -0,0 +1,8 @@
+from calc import *
+
+a = 14
+b = 10
+c = add(a, b)
+print(c)
+
+
---
+++ calc.py
@@ -0,0 +1,15 @@
+
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+def multi(x, y):
+    return x * y
+
+
+def div(x, y):
+    return x / y
