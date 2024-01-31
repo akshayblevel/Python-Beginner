a719870f81b2a6e2b46c782c474b9cdd380ea235
Akshay Patel <akshayblevel@gmail.com>
2024-01-31 19:53:12 +0530

    Coding Question

---
+++ 70_codingquestion.py
@@ -0,0 +1,11 @@
+# What's the expected output of the following code?
+
+a = 5
+# a = a + a
+print(a)
+
+# Output
+# 5
+
+# Hint
+# The a = a + a part of code is commented, and the interpreter ignores this line.
