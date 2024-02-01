0a98de5a39dc9165ae6b34d2dcee0ec8bb013f32
Akshay Patel <akshayblevel@gmail.com>
2024-02-01 15:17:40 +0530

    Coding Question

---
+++ 71_codingquestion.py
@@ -0,0 +1,12 @@
+# What's the expected output of the following code?
+
+a = 1  # + 5
+a = a + a
+# a = a + 1
+print(a)
+
+# Output
+# 2
+
+# Hint
+# The interpreter ignores the code lines preceded by the # symbol.
