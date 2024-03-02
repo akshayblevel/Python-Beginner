55e7316918d28f78a5e5b03e52ec386be0d21cba
Akshay Patel <akshayblevel@gmail.com>
2024-03-02 19:24:36 +0530

    Coding Question

---
+++ 95_codingquestion.py
@@ -0,0 +1,9 @@
+# What is the expected result of the following code?
+
+lst = ['a', 'b', 'c', 'd', 'e']
+print(lst[10:])
+
+# The above code will output [], and will not result in an IndexError.
+
+# OUTPUT
+# []
